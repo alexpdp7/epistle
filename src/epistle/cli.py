823441a44484bd61422f74bc9c8b3df656b29e91
alex <alex@pdp7.net>
2024-05-25 17:42:12 +0000

    Check for new messages and quit

--- src/epistle/cli.py
+++ src/epistle/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 from epistle import notmuch
 
@@ -6,9 +7,28 @@ from epistle import notmuch
 def watch(args):
     nm = notmuch.Notmuch()
     unread_messages = nm.unread_messages()
+
+    seen_ids = set()
+
     for message in sorted(unread_messages, key=lambda m: m.timestamp):
         if not message.in_trash:
             print(message.line)
+            seen_ids.add(message.id)
+
+    new_messages = False
+
+    print()
+    print("Waiting for new messages...")
+    print()
+
+    while not new_messages:
+        unread_messages = nm.unread_messages()
+        for message in unread_messages:
+            if message.id not in seen_ids and not message.in_trash:
+                print(message.line)
+                new_messages = True
+        if not new_messages:
+           time.sleep(10)
 
 
 def main():
